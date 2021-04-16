64e62a7b375eee0875d5a99404dcbaee3c622869
rodolfo-at-349255136144 <ffrojas.19@gmail.com>
2021-04-16 17:26:46 -0600

    setup default game parameters

--- minesweeper-api/src/main/java/org/clone/minesweeper/CloneMinesweeperApplication.java
+++ minesweeper-api/src/main/java/org/clone/minesweeper/CloneMinesweeperApplication.java
@@ -1,13 +1,15 @@
 package org.clone.minesweeper;
 
+import org.clone.minesweeper.config.GameParameterProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GameParameterProperties.class)
 public class CloneMinesweeperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloneMinesweeperApplication.class, args);
 	}
-
 }
---
+++ minesweeper-api/src/main/java/org/clone/minesweeper/config/GameParameterProperties.java
@@ -0,0 +1,29 @@
+package org.clone.minesweeper.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "game.match.parameters")
+public class GameParameterProperties {
+    private Integer rows;
+    private Integer cells;
+    private Integer bombs;
+
+    public Integer getRows() {
+        return rows;
+    }
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+    public Integer getCells() {
+        return cells;
+    }
+    public void setCells(Integer cells) {
+        this.cells = cells;
+    }
+    public Integer getBombs() {
+        return bombs;
+    }
+    public void setBombs(Integer bombs) {
+        this.bombs = bombs;
+    }
+}
