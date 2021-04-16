c89d3e31203abb0d5925e58e1cfe9e0c6c42a755
rodolfo-at-349255136144 <ffrojas.19@gmail.com>
2021-04-16 11:37:56 -0600

    initial project setup

---
+++ minesweeper-api/src/main/java/org/clone/minesweeper/CloneMinesweeperApplication.java
@@ -0,0 +1,13 @@
+package org.clone.minesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneMinesweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneMinesweeperApplication.class, args);
+	}
+
+}
---
+++ minesweeper-api/src/test/java/org/clone/minesweeper/CloneMinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clone.minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneMinesweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
