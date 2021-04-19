95a81ff71c14a06a6c15e2cf62d4be278064fd6f
rodolfo-at-349255136144 <ffrojas.19@gmail.com>
2021-04-18 22:55:24 -0600

    api documentation and actuator configuration

--- minesweeper-api/src/main/java/org/clone/minesweeper/exception/ControllerExceptionHandler.java
+++ minesweeper-api/src/main/java/org/clone/minesweeper/exception/ControllerExceptionHandler.java
@@ -1,25 +1,28 @@
 package org.clone.minesweeper.exception;
 
-import org.clone.minesweeper.exception.ApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(value = ApiException.class)
-    public ResponseEntity<String> apiExceptionHandler(ApiException e) {
+    public ResponseEntity<Map<String,String>> apiExceptionHandler(ApiException e) {
         HttpStatus httpStatus = e.getStatus();
         if(httpStatus != null) {
-            return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+            return ResponseEntity.status(e.getStatus()).body(Map.of("message",e.getMessage()));
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
-    @ExceptionHandler(value = Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void generalExceptionHandler() { }
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,String> badRequestHandler(IllegalArgumentException e) {
+        return Map.of("message",e.getMessage());
+    }
 }
